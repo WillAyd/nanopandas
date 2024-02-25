fa6c5e8b072801b84db86f6c6463675d47024e0c
Will Ayd <william.ayd@icloud.com>
2024-02-25 12:16:37 -0500

    More __getitem__ introspection

--- src/nanopandas/algorithms/generic.hpp
+++ src/nanopandas/algorithms/generic.hpp
@@ -10,6 +10,7 @@
 #include <vector>
 
 #include <nanobind/nanobind.h>
+#include <nanobind/ndarray.h>
 #include <utf8proc.h>
 
 #include "../array_types.hpp"
@@ -142,13 +143,14 @@ T FromFactorized([[maybe_unused]] const T &self, const Int64Array &locs,
 }
 
 template <typename T>
-std::optional<typename T::ScalarT> GetItemDunder(const T &self, int64_t i) {
+auto GetItemDunderInternal(const T &self, int64_t index)
+    -> std::optional<typename T::ScalarT> {
   const auto n = self.array_view_.get()->length;
-  if ((i >= n) || (i < -n)) {
-    throw std::range_error("index out of bounds!");
+  if ((index >= n) || (index < -n)) {
+    throw std::out_of_range("index out of bounds");
   }
-  const auto idx = i >= 0 ? i : n + i;
 
+  const auto idx = index >= 0 ? index : n + index;
   if (ArrowArrayViewIsNull(self.array_view_.get(), idx)) {
     return std::nullopt;
   }
@@ -158,12 +160,121 @@ std::optional<typename T::ScalarT> GetItemDunder(const T &self, int64_t i) {
   } else if constexpr (std::is_same_v<T, StringArray>) {
     const auto value =
         ArrowArrayViewGetStringUnsafe(self.array_view_.get(), idx);
+
     return
         typename T::ScalarT{value.data, static_cast<size_t>(value.size_bytes)};
   } else {
     // see https://stackoverflow.com/a/64354296/621736
     static_assert(!sizeof(T), "__getitem__ not implemented for type");
   }
+}
+
+template <typename T>
+auto GetItemDunder(const T &self, nb::object indexer) -> nb::object {
+
+  int64_t i;
+  if (nb::try_cast(indexer, i, false)) {
+    if (const auto result = GetItemDunderInternal(self, i)) {
+      if constexpr (std::is_same_v<T, BoolArray>) {
+        return nb::bool_(*result);
+      } else if constexpr (std::is_same_v<T, Int64Array>) {
+        return nb::int_(*result);
+      } else if constexpr (std::is_same_v<T, StringArray>) {
+        return nb::str(result->data(), result->size());
+      } else {
+        // see https://stackoverflow.com/a/64354296/621736
+        static_assert(!sizeof(T), "__getitem__ not implemented for type");
+      }
+    } else {
+      return nb::none();
+    }
+  }
+
+  // At this point we are working with an iterable
+  // TODO: we are falling back to return Python containers, but ideally we
+  // should still return T wrapped as a Python object
+  nb::list result{};
+
+  std::vector<std::optional<int64_t>> values;
+  if (nb::try_cast(indexer, values, false)) {
+    for (const auto idx : values) {
+      if (idx) {
+        if (const auto value = GetItemDunderInternal(self, *idx)) {
+          if constexpr (std::is_same_v<T, BoolArray>) {
+            result.append(nb::bool_(*value));
+          } else if constexpr (std::is_same_v<T, Int64Array>) {
+            result.append(nb::int_(*value));
+          } else if constexpr (std::is_same_v<T, StringArray>) {
+            result.append(nb::str(value->data(), value->size()));
+          } else {
+            // see https://stackoverflow.com/a/64354296/621736
+            static_assert(!sizeof(T), "__getitem__ not implemented for type");
+          }
+        } else {
+          result.append(nb::none());
+        }
+      } else {
+        result.append(nb::none());
+      }
+    }
+
+    return result;
+  }
+
+  /*
+  nb::ndarray<const int64_t, nb::ndim<1>> array;
+  if (nb::try_cast(indexer, array, false)) {
+    auto v = array.view();
+    for (size_t idx = 0; idx < v.shape(0); idx++) {
+      if (const auto value = GetItemDunderInternal(self, idx)) {
+        if constexpr (std::is_same_v<T, BoolArray>) {
+          result.append(nb::bool_(*value));
+        } else if constexpr (std::is_same_v<T, Int64Array>) {
+          result.append(nb::int_(*value));
+        } else if constexpr (std::is_same_v<T, StringArray>) {
+          result.append(nb::str(value->data(), value->size()));
+        } else {
+          // see https://stackoverflow.com/a/64354296/621736
+          static_assert(!sizeof(T), "__getitem__ not implemented for type");
+        }
+      } else {
+        result.append(nb::none());
+      }
+    }
+
+    return result;
+  }
+  */
+
+  nb::ndarray<const bool, nb::ndim<1>> array;
+  if (nb::try_cast(indexer, array, false)) {
+    auto v = array.view();
+    for (size_t idx = 0; idx < v.shape(0); idx++) {
+      const auto should_index = v(idx);
+      if (should_index) {
+        if (const auto value = GetItemDunderInternal(self, idx)) {
+          if constexpr (std::is_same_v<T, BoolArray>) {
+            result.append(nb::bool_(*value));
+          } else if constexpr (std::is_same_v<T, Int64Array>) {
+            result.append(nb::int_(*value));
+          } else if constexpr (std::is_same_v<T, StringArray>) {
+            result.append(nb::str(value->data(), value->size()));
+          } else {
+            // see https://stackoverflow.com/a/64354296/621736
+            static_assert(!sizeof(T), "__getitem__ not implemented for type");
+          }
+        } else {
+          result.append(nb::none());
+        }
+      }
+    }
+
+    return result;
+  }
+
+  throw std::out_of_range(
+      "only integers, slices (`:`), ellipsis (`...`), numpy.newaxis "
+      "(`None`) and integer or boolean arrays are valid indices");
 }
 
 template <typename T> BoolArray EqDunder(const T &self, const T &other) {
